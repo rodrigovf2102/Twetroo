f1c97b971e4a6beb50d4cba1ec6658ab8ffbf23a
Rodrigo <rodrigovf2102@gmail.com>
2022-08-29 12:05:01 -0300

    validacao de dados, status codes de posts, get tweets/username adicionados

--- index.js
+++ index.js
@@ -7,43 +7,60 @@ const server = express();
 server.use(cors());
 server.use(express.json());
 
-server.get('/sign-up', (req, res) => {
-    res.send("lalala");
-    console.log(usuarios);
-});
 server.post('/sign-up', (req, res) => {
-    if (req.body.username !== undefined) {
-        if (req.body.avatar !== undefined) {
-            if (req.body.avatar.includes("https://")) {
-                usuarios.push({ ...req.body });
-            }
-        }
+    const { username, avatar } = req.body;
+    if (username === undefined || avatar === undefined) {
+        return res.status(400).send("Todos os campos são obrigatórios!");
     }
-    res.send("OK");
+    if (!avatar.includes("https://")) {
+        return res.sendStatus(401);
+    }
+    usuarios.push({ username,avatar });
+    res.status(201).send("OK");
 })
 
 server.post('/tweets', (req, res) => {
-    if (req.body.username !== undefined) {
-        if (req.body.tweet !== undefined) {
-            tweets.push({ ...req.body });
-        }
+    const { username, tweet } = req.body;
+    if (username === undefined || tweet === undefined) {
+        return res.status(400).send("Todos os campos são obrigatórios!");
     }
-    res.send("OK");
+    if((usuarios.find(usuario=>usuario.username === username))===undefined){
+        return res.sendStatus(401);
+    }
+    tweets.push({ username, tweet });
+    res.status(201).send("OK");
 })
 
 server.get('/tweets', (req, res) => {
     const lastTweets = [];
     const tweet = {};
     for (let i = 0; i < 10; i++) {
-        if(tweets[i] !== undefined) {
+        if (tweets[i] !== undefined) {
             tweet.username = tweets[i].username;
             tweet.tweet = tweets[i].tweet;
             let usuario = usuarios.find(usuario => usuario.username === tweet.username);
             tweet.avatar = usuario.avatar;
-            lastTweets.push({...tweet});
-        } 
+            lastTweets.push({ ...tweet });
+        }
     }
     res.send(lastTweets);
 })
+
+server.get('/tweets/:username', (req, res) => {
+    const username = req.params.username;
+    const userTweets = tweets.filter(tweet => tweet.username === username);
+    let user;
+    if(userTweets.length!==0){
+        user = usuarios.find(usuario=>usuario.username === userTweets[0].username);
+    }
+    if(userTweets.length === 0){ 
+        return res.status(400).send("Usuário não encontrado")
+    }
+    for(let i=0;i<userTweets.length;i++){
+        userTweets[i].avatar = user.avatar;
+    }
+    res.send(userTweets);
+})
+
 
 server.listen(5000);
